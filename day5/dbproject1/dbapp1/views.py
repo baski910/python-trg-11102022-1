9ff8499e06bcfc7af3a5d2070c250901d14ab243
baski910 <baskar910@gmail.com>
2022-10-17 13:36:21 +0530

    fetch from table

--- day5/dbproject1/dbapp1/views.py
+++ day5/dbproject1/dbapp1/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
 def home(request):
-    return render(request,'dbapp1/home.html')
+    books = models.Book.objects.all()
+    return render(request,'dbapp1/home.html',{'books':books})
